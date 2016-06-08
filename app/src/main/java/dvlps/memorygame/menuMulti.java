6dfeabd380be6b4b4afb33308be9aab763275724
Johann Hospice <johann.hospice@netdevices.fr>
2016-06-08 11:50:56 +0200

    Link the multiplayer view with the multiplayer menu

--- app/src/main/java/dvlps/memorygame/menuMulti.java
+++ app/src/main/java/dvlps/memorygame/menuMulti.java
@@ -1,27 +1,31 @@
 package dvlps.memorygame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class menuMulti extends Activity {
 
-    private Button btn_local, btn_online;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_multi);
 
-        btn_local = (Button) findViewById(R.id.btn_local);
-
-        btn_local.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener onClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(menuMulti.this, MainActivity.class);
+                intent.putExtra("vitesse", (double)1);
+                intent.putExtra("niveau", 1);
+                intent.putExtra("nbPlayer", 2);
+                startActivity(intent);
             }
-        });
+        };
+        findViewById(R.id.btn_local).setOnClickListener(onClick);
+        findViewById(R.id.btn_online).setOnClickListener(onClick);
     }
 
 
